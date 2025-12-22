201bfc6bbf5945445e74aa8041e171b4773ba012
Abhishek Shinde <abhishek.shinde01@globant.com>
2025-12-22 13:40:50 +0530

    update pacakge file

--- src/config/db.js
+++ src/config/db.js
@@ -14,25 +14,6 @@
 const { Sequelize } = require("sequelize");
 require("dotenv").config();
 
-// Create a Sequelize instance
-// const sequelize = new Sequelize(
-//   process.env.DB_NAME, // database name
-//   process.env.DB_USER, // database username
-//   process.env.DB_PASSWORD, // database password
-//   {
-//     host: process.env.DB_HOST,
-//     dialect: "mysql", // use MySQL
-//     logging: false,
-//     dialectOptions: {
-//       ssl: {
-//         require: true,
-//         rejectUnauthorized: false, // Necessary to bypass CA file requirements
-//       },
-//     },
-//   }
-// );
-
-// const { Sequelize } = require('sequelize');
 
 const sequelize = new Sequelize(
   process.env.DB_NAME, 
