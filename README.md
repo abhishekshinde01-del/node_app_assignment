67cd2c30cfe4270ce936c6a9473a6a0d27e3a263
Abhishek Shinde <abhishek.shinde01@globant.com>
2025-12-22 15:19:07 +0530

    update pacakge file

--- README.md
+++ README.md
@@ -4,7 +4,3 @@
 Step 1 - Run apllication using this command -> node ./src/server.js
 Step 2 - Run this command on chrome so you will get the swagger - http://mysql-6a5fb7-node-assignment-db.k.aivencloud.com:23574/api-docs/
 
-
-
-
-http://mysql-6a5fb7-node-assignment-db.k.aivencloud.com:23570
--- src/server.js
+++ src/server.js
@@ -3,7 +3,7 @@ const sequelize = require('./config/db');
 
 sequelize.sync().then(() => {
   console.log('Database connected');
-  app.listen(3000, () => {
-    console.log('Server running at http://localhost:3000');
+  app.listen(23570, () => {
+    console.log(`Server running at http://mysql-6a5fb7-node-assignment-db.k.aivencloud.com:${23570}`);
   });
 });
