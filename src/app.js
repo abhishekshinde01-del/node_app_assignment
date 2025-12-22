ad878cb96a331ca34484e9a2821c51a34b44a989
Abhishek Shinde <abhishek.shinde01@globant.com>
2025-12-22 15:09:35 +0530

    update pacakge file

--- src/app.js
+++ src/app.js
@@ -6,13 +6,7 @@ const app = express()
 const userRoute = require('./routes/routes')
 
 app.use(express.json())
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(specs, {
-  customCssUrl: 'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui.min.css',
-  customJs: [
-    'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui-bundle.js',
-    'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui-standalone-preset.js'
-  ]
-}));
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 app.use('/employe' , userRoute)
 
 module.exports = app;
